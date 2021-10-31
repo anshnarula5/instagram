6aec17f6d42e5a955eb559a4b429f154c399d990
Ansh Narula <2019210@iiitdmj.ac.in>
2021-10-31 11:45:41 +0530

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const connectDB = require("./config/db.js");
 
 const express = require("express");
 const cors = require("cors");
-const path = require("path")
+const path = require("path");
 
 const app = express();
 
@@ -12,7 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(cors());
 
-
 //routes
 
 app.use("/api/user", require("./routes/user"));
@@ -20,11 +19,15 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/profile", require("./routes/profile"));
 app.use("/api/posts", require("./routes/post"));
 
-    app.use(express.static("client/build"))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    })
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-const port = process.env.PORT || 5000
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-app.listen(port, () => console.log("Running on 5000"));
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log("Running on 5000"));
