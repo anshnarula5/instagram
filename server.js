6016cbffe81497005868954238671bdbca307746
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-01 15:31:01 +0530

    deploy

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log("Running on 5000"));
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
