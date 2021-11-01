e1823943e36860aca09f0cfe38ccb75151918b3b
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-01 16:23:16 +0530

    deploy

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-const connectDB = require("./config/db")
+const mongoose = require("mongoose");
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
 const app = express();
 
-// require("dotenv").config()
+require("dotenv").config()
 
-// const dbUrl = process.env.DB_URL
+const dbUrl = process.env.DB_URL
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(cors());
+
+
+const connectDB = async() => {
+  try {
+      await mongoose.connect(dbUrl)
+      console.log("Mongoose running")
+  } catch (error) {
+      console.log(error)
+      process.exit(1)
+  }
+};
 
 connectDB();
 
@@ -22,14 +34,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/profile", require("./routes/profile"));
 app.use("/api/posts", require("./routes/post"));
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("/", (req, res) => res.send("Hello"))
 
 const PORT = process.env.PORT || 5000;
 
