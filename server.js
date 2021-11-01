b450e067306e239627d76607d9bd770b341fe523
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-01 14:57:49 +0530

    deploy

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const connectDB = require("./config/db.js");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/insta"
 
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-
 const app = express();
 
 const mongoose = require("mongoose");
 
-const db = async() => {
+const connectDB = async() => {
     try {
-        await mongoose.connect("mongodb+srv://ansh:ansh123@cluster0.pl7m9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        await mongoose.connect(dbUrl)
         console.log("Mongoose running")
     } catch (error) {
         console.log(error)
@@ -19,7 +22,7 @@ const db = async() => {
     }
 };
 
-db()
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -43,4 +46,4 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`))
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
